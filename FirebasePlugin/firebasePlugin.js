8bbe6263216436684b43a20199b343342ee37f75
KYPost <118707235+KYPost@users.noreply.github.com>
2024-04-11 15:22:28 +0800

    initial commit

---
+++ FirebasePlugin/firebasePlugin.js
@@ -0,0 +1,14 @@
+// Firebase的金鑰
+const firebaseConfig = {
+    apiKey: "AIzaSyAOykG5KTNAU9rwYXMV8heZDHeBx5vd7-o",
+    authDomain: "gatest-e37d7.firebaseapp.com",
+    databaseURL: "https://gatest-e37d7-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "gatest-e37d7",
+    storageBucket: "gatest-e37d7.appspot.com",
+    messagingSenderId: "94652725594",
+    appId: "1:94652725594:web:db90d880f45079cb2e5b63",
+    measurementId: "G-VXGY78PVZG"
+};
+
+// initialize firebase
+firebase.initializeApp(firebaseConfig);
